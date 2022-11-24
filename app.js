0156ab3502abfed0d7267a69ceba3f1a961ded75
Rezaul <mdrezaulkarrim@gmail.com>
2022-11-24 19:44:35 +0600

    implemented checking routes for different user

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const { MongoClient, ServerApiVersion } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const jwt = require("jsonwebtoken");
 
 const port = process.env.PORT || 5000;
 
@@ -9,6 +10,24 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+// veryfying jwt token middleware
+const verifyJWT = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(401).send({ message: "Unauthorized access" });
+  }
+
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(403).send({ message: "Forbidden access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
 
 const run = async function () {
   try {
@@ -21,6 +40,16 @@ const run = async function () {
 
     const categoriesCollection = client.db("eStall").collection("categories");
     const usersCollection = client.db("eStall").collection("user");
+
+    // creating jwt token
+
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "90d",
+      });
+      res.send({ token });
+    });
 
     // get category by id
     app.get("/categories/:id", async (req, res) => {
@@ -35,8 +64,44 @@ const run = async function () {
     app.post("/users", async (req, res) => {
       const doc = req.body;
 
+      const query = {
+        name: req.body.name,
+        email: req.body.email,
+      };
+      const alreadyLoggedIn = await usersCollection.findOne(query);
+
+      if (alreadyLoggedIn)
+        return res.send({ message: "User already logged in!" });
+
       const result = await usersCollection.insertOne(doc);
       res.send(result);
+    });
+
+    // check buyer
+    app.get("/users/buyer", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+
+      const user = await usersCollection.findOne(query);
+      res.send({ isBuyer: user?.role === "buyer" });
+    });
+
+    // check seller
+    app.get("/users/seller", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+
+      const user = await usersCollection.findOne(query);
+      res.send({ isSeller: user?.role === "seller" });
+    });
+
+    // check admin
+    app.get("/users/admin", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+
+      const user = await usersCollection.findOne(query);
+      res.send({ isAdmin: user?.role === "admin" });
     });
   } finally {
   }
